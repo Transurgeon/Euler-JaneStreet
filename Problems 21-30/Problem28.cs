33bf3e18c037a4395324568b28472b2818525711
Transurgeon <petertakayuki@gmail.com>
2021-08-30 22:26:58 -0400

    finish q28. quicksolve

--- Problems 21-30/Problem28.cs
+++ Problems 21-30/Problem28.cs
@@ -6,5 +6,32 @@ namespace ProjectEuler.Net.Problems_21_30
 {
     class Problem28
     {
+        /*        static void Main(string[] args)
+        {
+            findSumDiagonalSpiral(1001);
+        }
+        static void findSumDiagonalSpiral(int grid)
+        {
+            var sum = new BigInteger(1);
+            int count = 0;
+            int skip = 2;
+            int a = 3;
+            while (a <= grid * grid)
+            { 
+                count++;
+                if (count == 4)
+                {
+                    count = 0;
+                    skip += 2;
+                    Console.WriteLine(skip);
+                }
+                sum += a;
+                a += skip;
+               
+            }
+            Console.WriteLine(sum);
+
+        }
+        */
     }
 }
--- Program.cs
+++ Program.cs
@@ -11,64 +11,33 @@ namespace ProjectEuler.Net
     {
         static void Main(string[] args)
         {
-            findLongestQuadraticPrimes(1000);
-            //Console.WriteLine(getLongestPrimeSeq(-79, 1601));
+            findSumDiagonalSpiral(1001);
         }
-        static void findLongestQuadraticPrimes(int limit)
+        static void findSumDiagonalSpiral(int grid)
         {
-            int largest = 0;
-            int a = 0; int b = 0;
-           for (int i = 1; i < limit; i+=2)
-            {
-                for (int j = 1; j < limit; j +=2)
+            var sum = new BigInteger(1);
+            int count = 0;
+            int skip = 2;
+            int a = 3;
+            while (a <= grid * grid)
+            { 
+                count++;
+                if (count == 4)
                 {
-                    if (getLongestPrimeSeq(-i,j) > largest)
-                    {
-                        a = -i; b = j;
-                        largest = getLongestPrimeSeq(-i, j);
-                        Console.WriteLine((a * b) + ", " + a + ", " + b + ", " + largest);
-                    }
-                    else if (getLongestPrimeSeq(i, j) > largest)
-                    {
-                        a = i; b = j;
-                        largest = getLongestPrimeSeq(i, j);
-                        Console.WriteLine((a * b) + ", " + a + ", " + b + ", " + largest);
-                    }
+                    count = 0;
+                    skip += 2;
+                    Console.WriteLine(skip);
                 }
+                sum += a;
+                a += skip;
+               
             }
-            Console.WriteLine((a*b)+", "+ a + ", " + b + ", " + largest);
-        }
-        
-        static int getLongestPrimeSeq(int a, int b)
-        {
-            int n = 0;
-            do
-            {
-                if (isPrime(n * n + n * a + b))
-                    n++;
-                else
-                    return n;
+            Console.WriteLine(sum);
 
-            } while (true);
-        }
-        static Boolean isPrime(long a)
-        {
-            if (a < 0)
-                return false;
-            if (a % 2 == 0)
-                return false;
-            for (long i = 3; i <= Math.Sqrt(a); i += 2)
-            {
-                if (a % i == 0)
-                {
-                    return false;
-                }
-            }
-            return true;
         }
     }
-
 }
+
 
     
 
