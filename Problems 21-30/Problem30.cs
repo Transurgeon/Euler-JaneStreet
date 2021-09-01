9afabc0340a683da5b8d7a0baa3fec45b3937529
Transurgeon <petertakayuki@gmail.com>
2021-08-31 23:07:07 -0400

    finished q30. (quite easy)

--- Problems 21-30/Problem30.cs
+++ Problems 21-30/Problem30.cs
@@ -6,5 +6,36 @@ namespace ProjectEuler.Net.Problems_21_30
 {
     class Problem30
     {
+        /*         static void Main(string[] args)
+        {
+            
+            find5thDigitPower();
+        }
+        static void find5thDigitPower()
+        {
+            int sum = 0;
+            for (int i = 100; i<300000; i++)
+            {
+                if (is5thDigitPow(i))
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+        static bool is5thDigitPow(int num)
+        {
+            int sum = 0;
+            int final = num;
+            while(num!= 0)
+            {
+                sum += (int)Math.Pow((num %10), 5);
+                num = num / 10;
+            }
+            if (sum == final)
+                return true;
+            else
+            return false;
+        } */
     }
 }
--- Program.cs
+++ Program.cs
@@ -11,59 +11,34 @@ namespace ProjectEuler.Net
     {
         static void Main(string[] args)
         {
-             Console.WriteLine(findNthPermutation(9,999999));
-            //Console.WriteLine(isPermutation(1235476890));
-        }
-        static string findNthPermutation(int lastDigit, int n)
-        {
             
-            if ((lastDigit < 0 && lastDigit > 9) || (n < 1 && n > getFactorial(lastDigit + 1)))
-                return "!";
-            string digits = "";
-            for (int i = 0; i<=lastDigit; i++)
-            {
-                digits += i;
-            }
-            int index;
-            string nthPerm = "";
-            string result = "";
-            for (int i = lastDigit; i>=0; i--)
-            {
-                index = n / getFactorial(i);
-                n = n - (index*getFactorial(i));
-                Console.WriteLine(index);
-                nthPerm = digits.Substring(index, 1);
-                result += digits.Substring(index, 1);
-                digits = digits.Replace(nthPerm.ToString(),"");
-                Console.WriteLine(digits);
-            }
-            return result;
+            find5thDigitPower();
         }
-      /*  static bool isPermutation(long num)
+        static void find5thDigitPower()
         {
-            String str;
-            if (num < 1000000000)
-                str = "0" + num;
+            int sum = 0;
+            for (int i = 100; i<300000; i++)
+            {
+                if (is5thDigitPow(i))
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+        static bool is5thDigitPow(int num)
+        {
+            int sum = 0;
+            int final = num;
+            while(num!= 0)
+            {
+                sum += (int)Math.Pow((num %10), 5);
+                num = num / 10;
+            }
+            if (sum == final)
+                return true;
             else
-                str = ""+num;
-
-            for (int i = 9; i>=0; i--)
-            {
-                str = str.Replace(i.ToString(),"");
-                if (str.Length != i)
-                    return false;
-            }
-            return true;
-        } */
-
-        static int getFactorial(int n)
-        {
-            int product = 1;
-            for (int i = 2; i<=n; i++)
-            {
-                product *= i;
-            }
-            return product;
+            return false;
         }
     }
 }
