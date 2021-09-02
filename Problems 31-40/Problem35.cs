5d0dad334a4d5f3df47616f6469d2fd9ca6379c6
Transurgeon <petertakayuki@gmail.com>
2021-09-01 23:04:21 -0400

    finished q35!

--- Problems 31-40/Problem35.cs
+++ Problems 31-40/Problem35.cs
@@ -6,5 +6,56 @@ namespace ProjectEuler.Net.Problems_31_40
 {
     class Problem35
     {
+        /*         static void Main(string[] args)
+        {
+             findCircularPrimes(1000000);
+           //Console.WriteLine( isCircularPrime(29));
+        }
+        static void findCircularPrimes(int limit)
+        {
+            int count = 0;
+            int j = 0;
+            for (int i = 2; i < limit; i++)
+            {
+                if (isPrime(i))
+                {
+                    if (isCircularPrime(i))
+                      count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+
+        static bool isCircularPrime(int prime)
+        {
+            if (!isPrime(prime))
+                return false;
+            if (prime < 10)
+                return true;
+            int length = (int)Math.Log10(prime);
+            int i = (prime % 10) * (int)Math.Pow(10, length) + prime/10;
+            do
+            {
+                if (!isPrime(i))
+                    return false;
+                i = (i % 10) * (int)Math.Pow(10, length) + i / 10;
+            } while (i != prime);
+            return true;
+        }
+        static Boolean isPrime(int a)
+        {
+            if (a < 2)
+                return false;
+            if (a % 2 == 0 && a != 2)
+                return false;
+            for (int i = 3; i <= Math.Sqrt(a); i += 2)
+            {
+                if (a % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }*/
     }
 }
--- Program.cs
+++ Program.cs
@@ -11,38 +11,54 @@ namespace ProjectEuler.Net
     {
         static void Main(string[] args)
         {
-            findDigitFactorials();
+             findCircularPrimes(1000000);
+           //Console.WriteLine( isCircularPrime(29));
         }
-        static void findDigitFactorials()
+        static void findCircularPrimes(int limit)
         {
-            int sum = 0;
-            for (int i = 3; i<= 2540160; i++)
+            int count = 0;
+            int j = 0;
+            for (int i = 2; i < limit; i++)
             {
-                if (isSumOfFactorial(i))
-                    sum += i;
+                if (isPrime(i))
+                {
+                    if (isCircularPrime(i))
+                      count++;
+                }
             }
-            Console.WriteLine(sum);
+            Console.WriteLine(count);
         }
 
-        static bool isSumOfFactorial(int num)
+        static bool isCircularPrime(int prime)
         {
-            int sum = 0;
-            int n = num;
-            while (n!=0)
+            if (!isPrime(prime))
+                return false;
+            if (prime < 10)
+                return true;
+            int length = (int)Math.Log10(prime);
+            int i = (prime % 10) * (int)Math.Pow(10, length) + prime/10;
+            do
             {
-                sum += getFactorial(n % 10);
-                n = n / 10;
-            }
-            return sum == num;
+                if (!isPrime(i))
+                    return false;
+                i = (i % 10) * (int)Math.Pow(10, length) + i / 10;
+            } while (i != prime);
+            return true;
         }
-        static int getFactorial(int num)
+        static Boolean isPrime(int a)
         {
-            int product = 1;
-            for (int i = num; i>1; i--)
+            if (a < 2)
+                return false;
+            if (a % 2 == 0 && a != 2)
+                return false;
+            for (int i = 3; i <= Math.Sqrt(a); i += 2)
             {
-                product *= i;
+                if (a % i == 0)
+                {
+                    return false;
+                }
             }
-            return product;
+            return true;
         }
     }
 }
