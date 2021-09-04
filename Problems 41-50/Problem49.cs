aa08dae7848f6a2e0653ed304b6c9a6d0354476a
Transurgeon <petertakayuki@gmail.com>
2021-09-04 17:16:25 -0400

    done q49, under 1 min. Can be a lot more efficient

--- Problems 41-50/Problem49.cs
+++ Problems 41-50/Problem49.cs
@@ -6,5 +6,69 @@ namespace ProjectEuler.Net.Problems_41_50
 {
     class Problem49
     {
+        /*        static void Main(string[] args)
+        {
+            Console.WriteLine(findHighestConsecutivePrimeSum(1000000));
+        }
+
+        static List<int> primes = new List<int>();
+
+        static int findHighestConsecutivePrimeSum(int high)
+        {
+            int largest = 0;
+            int largestP = 0;
+            for (int i = 2; i<= high; i++)
+            {
+                if (isPrime(i))
+                {
+                    primes.Add(i);
+                    if (findConsecutivePrime(i, largest) > largest)
+                    {
+                        largest = findConsecutivePrime(i, largest);
+                        largestP = i;
+                        Console.WriteLine(largestP + ", " + largest);
+                    }
+                }
+            }
+            return largestP;
+        }
+
+        static int findConsecutivePrime(int prime, int largest)
+        {
+            int pos = primes.IndexOf(prime) - largest;
+            int n = 0;
+            int sum = 0;
+            while (n < pos)
+            {
+                for (int i = n; i<=pos; i++)
+                {
+                    sum += primes[i];
+                    if (sum > prime)
+                    {
+                        break;
+                    }
+                    else if (sum == prime)
+                        return i - n+1;
+                }
+                sum = 0;
+                n++;
+            }
+            return 0;
+        }
+        static Boolean isPrime(long a)
+        {
+            if (a < 2)
+                return false;
+            if (a % 2 == 0 && a != 2)
+                return false;
+            for (int i = 3; i <= Math.Sqrt(a); i += 2)
+            {
+                if (a % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        } */
     }
 }
