bde6a340db216ffacd1cf22bb09209f5ffaf44df
Transurgeon <petertakayuki@gmail.com>
2021-09-04 16:37:11 -0400

    finished q48 (doesnt work with Math.Pow)

--- Problems 41-50/Problem48.cs
+++ Problems 41-50/Problem48.cs
@@ -6,5 +6,31 @@ namespace ProjectEuler.Net.Problems_41_50
 {
     class Problem48
     {
+        /*         static void Main(string[] args)
+        {
+            Console.WriteLine(findSelfPowers(1000));
+        }
+
+        static BigInteger findSelfPowers(int n)
+        {
+            var sum = new BigInteger(0);
+            for (int i = 1; i<=n; i++)
+            {
+                sum += multiplyMod(i);
+                Console.WriteLine(i+","+sum);
+            }
+            return sum % 10000000000;
+        }
+
+        static BigInteger multiplyMod(int num)
+        {
+            BigInteger product = 1;
+            for (int i = 1; i<=num; i++)
+            {
+                product *= num;
+                product = product % 10000000000;
+            }
+            return product;
+        }*/
     }
 }
--- Program.cs
+++ Program.cs
@@ -11,53 +11,29 @@ namespace ProjectEuler.Net
     {
         static void Main(string[] args)
         {
-           Console.WriteLine(findDistinctPrimeFactors(4));
+            Console.WriteLine(findSelfPowers(1000));
         }
 
-        static List<int> primes = new List<int>();
-        static int findDistinctPrimeFactors(int disctint)
+        static BigInteger findSelfPowers(int n)
         {
-            int n = 2;
-            while (true)
+            var sum = new BigInteger(0);
+            for (int i = 1; i<=n; i++)
             {
-                if (isPrime(n))
-                    primes.Add(n);
-                else if (isN_Distinct(disctint,n) && isN_Distinct(disctint,n + 1) && isN_Distinct(disctint, n + 2) && isN_Distinct(disctint, n + 3))
-                    return n;
-                n++;
+                sum += multiplyMod(i);
+                Console.WriteLine(i+","+sum);
             }
+            return sum % 10000000000;
         }
-        static bool isN_Distinct(int disctint, int num)
+
+        static BigInteger multiplyMod(int num)
         {
-            int factorCount = 0;
-            for (int i = 0; i<primes.Count; i++)
+            BigInteger product = 1;
+            for (int i = 1; i<=num; i++)
             {
-                if (num %  primes[i] == 0) {
-                    do
-                    {
-                        num = num / primes[i];
-                    } while (num % primes[i] == 0);
-                    factorCount++;
-                }
+                product *= num;
+                product = product % 10000000000;
             }
-            if (factorCount == disctint)
-                return true;
-            return false;
-        }
-        static Boolean isPrime(long a)
-        {
-            if (a < 2)
-                return false;
-            if (a % 2 == 0 && a != 2)
-                return false;
-            for (int i = 3; i <= Math.Sqrt(a); i += 2)
-            {
-                if (a % i == 0)
-                {
-                    return false;
-                }
-            }
-            return true;
+            return product;
         }
     }
 }
