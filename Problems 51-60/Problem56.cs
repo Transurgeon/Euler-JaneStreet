e2bb62c6a4818856fbac74ed1dd8482f72601dd2
Transurgeon <petertakayuki@gmail.com>
2021-09-06 13:30:29 -0400

    finished q56 and moved q92,97 in different folder

--- Problems 51-60/Problem56.cs
+++ Problems 51-60/Problem56.cs
@@ -6,5 +6,40 @@ namespace ProjectEuler.Net.Problems_51_60
 {
     class Problem56
     {
+        /*         static void Main(string[] args)
+        {
+            Console.WriteLine(findMaximalDigitSum());
+        }
+        static int findMaximalDigitSum()
+        {
+            int largest = 0;
+            for (int i = 1; i<=100; i++)
+            {
+                for (int j = 1; j <= 100; j++)
+                {
+                    if (findDigitSum(i,j) > largest)
+                    {
+                        largest = findDigitSum(i,j);
+                        Console.WriteLine(i + "," + j+","+largest);
+                    }
+                }
+            }
+            return largest;
+        }
+        static int findDigitSum(int num1, int num2)
+        {
+            var product = new BigInteger(1);
+            for (int i = 1; i <= num2; i++)
+            {
+                product *= num1;
+            }
+            int digits = 0;
+            while (product!=0)
+            {
+                digits += int.Parse((product % 10).ToString());
+                product = product / 10;
+            }
+            return digits;
+        }*/
     }
 }
--- Program.cs
+++ Program.cs
@@ -11,21 +11,38 @@ namespace ProjectEuler.Net
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(findVeryLargeNon_Mersenne(28433,7830457));
+            Console.WriteLine(findMaximalDigitSum());
         }
-
-        static long findVeryLargeNon_Mersenne(int product, int power)
+        static int findMaximalDigitSum()
         {
-            long num = 1;
-            for (int i = 0; i < power;i++)
+            int largest = 0;
+            for (int i = 1; i<=100; i++)
             {
-                num *= 2;
-                num = num % 10000000000;
+                for (int j = 1; j <= 100; j++)
+                {
+                    if (findDigitSum(i,j) > largest)
+                    {
+                        largest = findDigitSum(i,j);
+                        Console.WriteLine(i + "," + j+","+largest);
+                    }
+                }
             }
-            num *= product;
-            num = num % 10000000000;
-            num++;
-            return num;
+            return largest;
+        }
+        static int findDigitSum(int num1, int num2)
+        {
+            var product = new BigInteger(1);
+            for (int i = 1; i <= num2; i++)
+            {
+                product *= num1;
+            }
+            int digits = 0;
+            while (product!=0)
+            {
+                digits += int.Parse((product % 10).ToString());
+                product = product / 10;
+            }
+            return digits;
         }
     }
 }
