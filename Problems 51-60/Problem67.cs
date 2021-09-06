2d98e53df211f11e4511e3065852c0ca3d84133d
Transurgeon <petertakayuki@gmail.com>
2021-09-06 13:32:41 -0400

    updated q67, added q63

---
+++ Problems 51-60/Problem67.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectEuler.Net.Problems_51_60
+{
+    class Problem67
+    {
+        //reference back to question 18 pls. The code from question 18 is actually solving this one.
+    }
+}
