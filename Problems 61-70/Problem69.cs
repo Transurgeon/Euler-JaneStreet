9327b20eee296b88057418b82ff8e34348c5d3da
Transurgeon <petertakayuki@gmail.com>
2021-10-21 15:14:51 -0400

    finished q69, very interesting. added folder for new questions (more!)

--- Problems 61-70/Problem69.cs
+++ Problems 61-70/Problem69.cs
@@ -6,5 +6,27 @@ namespace ProjectEuler.Net.Problems_61_70
 {
     class Problem69
     {
+
+        /*solved using pen and paper
+         using math theory, one can easily find that to achieve the greatest ratio between the numerator and the denominator
+         we will need either 1. a very big numerator or 2. a comparatively very small denominator or 3. both 1,2 simultaneously
+         
+        ----------- 
+        we can see that to achieve the lowest possible denominator for n, n should have the least amount of relative prime numbers
+        in other words we want a number that is a factor of primes. since this would accrue only prime numbers in the denominator. 
+
+        here is example of a function to find the highest n/phi(n) under a number: 'max
+
+        static long getRatio (long max) 
+        {
+        long value = 1;
+        do {
+        value *= the next prime number; ie: 2*3*5*7*11*13*17 = 510510, since multiplying by 19 would net us a value over 10^6
+        }(value < max)
+        return value/lastPrime;
+        }
+        It is quite interesting to note that the highest n/phi(n) is the highest composite of unique primes
+        while the lowest n/phi(n) is simply the highest prime, being relative prime to every non-prime number
+         */
     }
 }
--- Program.cs
+++ Program.cs
@@ -15,190 +15,21 @@ namespace ProjectEuler.Net
         {
             var watch = new System.Diagnostics.Stopwatch();
             //this is for reading txt files
-            string path = @"C:\Users\Admin\source\repos\ProjectEuler.Net\Problems 61-70\p054_poker.txt";
+            //string path = @"C:\Users\Admin\source\repos\ProjectEuler.Net\Problems 61-70\p054_poker.txt";
 
-            List<string> lines = new List<string>();
-            lines = File.ReadAllLines(path).ToList();
-            String input = "";
-            foreach (String line in lines)
-            {
-                input += line + "\n";
-            } 
+            //List<string> lines = new List<string>();
+            //lines = File.ReadAllLines(path).ToList();
+            //String input = "";
+            //foreach (String line in lines)
+            //{
+            //    input += line + "\n";
+            //} 
             //comment out till here if no need :)
             watch.Start();
-            Console.WriteLine(findAllHandValues(input));
-            //List<int> test = new List<int>();
-            //test.Add(1); test.Add(2);  test.Add(2); test.Add(14);test.Add(14);
-            //List<List<int>> values = getOccurenceAndValues(test);
-            //for (int i = 0; i < values.Count; i++)
-            //{
-            //    Console.WriteLine(values[i][0] + "," + values[i][1]);
-            //}
+
             watch.Stop();
 
             Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
-        }
-        static int findAllHandValues(String input)
-        {
-            int count = 0;
-            using (StringReader reader = new StringReader(input))
-            {
-                string hand = string.Empty;
-                do
-                {
-                    hand = reader.ReadLine();
-                    if (hand != null)
-                    {
-                        List<int> hand1 = getHandValue(hand.Substring(0, 15));
-                        List<int> hand2 = getHandValue(hand.Substring(15));
-                        int index = 0;
-                        do
-                        {
-                            if (hand1[index] > hand2[index])
-                                count++;
-                            index++;
-                        } while (hand1[index-1] == hand2[index-1]);
-                    }
-
-                } while (hand != null);
-            }
-            return count;
-        }
-        static List<int> getHandValue(String hand)
-        {
-            List<int> value = new List<int>();
-            List<int> nums = getNums(hand);
-            if (isFlush(hand))
-            {
-                if (!nums.Select((i, j) => i - j).Distinct().Skip(1).Any()) //isStraight
-                {
-                    if (hand.Contains('A') && hand.Contains('T'))
-                    {
-                        value.Add(9);
-                    }
-                    else
-                    {
-                        value.Add(8);
-                        value.Add(nums[nums.Count-1]);
-                    }
-                }
-                else {
-                    value.Add(5);
-                    nums.Reverse();
-                    value.AddRange(nums);
-                }
-            }
-            else
-            {
-                if (!nums.Select((i, j) => i - j).Distinct().Skip(1).Any()) //isStraight
-                {
-                    value.Add(4);
-                    value.Add(nums[nums.Count-1]);
-                }
-                else {
-                    List<List<int>> values= getOccurenceAndValues(nums);
-                    if (values[0][0] == 1)
-                    {
-                        value.Add(0);
-                    }
-                    else if (values[0][0] == 2)
-                    {
-                        //check if double pair ... return 1 or 2
-                        if (values[1][0] == 2)
-                        {
-                            value.Add(2);
-                        }
-                        else
-                        {
-                            value.Add(1);
-                        }
-                    }
-                    else if (values[0][0] == 3)
-                    {
-                        //check if full-house ... return 3 or 6
-                        if (values[1][0] == 2)
-                        {
-                            value.Add(6);
-                        }
-                        else
-                        {
-                            value.Add(3);
-                        }
-                    }
-                    else if (values[0][0] == 4)
-                    {
-                        value.Add(7);
-                    }
-                    for (int i = 0; i<values.Count; i++)
-                    {
-                        value.Add(values[i][1]);
-                    }
-                }
-            }
-            return value;
-        }
-        static bool isFlush(String input)
-        {
-            string suits = "";
-            for (int i = 0; i<input.Length; i++)
-            {
-                if (input[i] == 'C' || input[i] == 'H' || input[i] == 'D' || input[i] == 'S')
-                    suits += input[i];
-            }
-            suits = suits.Replace(suits[0],' ').Trim();
-            if (suits == "")
-                return true;
-            return false;
-        }
-        static List<int> getNums(String input)
-        {
-            List<int> nums = new List<int>();
-            for (int i = 0; i < input.Length; i++)
-            {
-                if (Char.IsDigit(input[i]))
-                    nums.Add(int.Parse(input[i]+""));
-                else
-                {
-                    switch (input[i])
-                    {
-                        case 'T':
-                            nums.Add(10);
-                            break;
-                        case 'J':
-                            nums.Add(11);
-                            break;
-                        case 'Q':
-                            nums.Add(12);
-                            break;
-                        case 'K':
-                            nums.Add(13);
-                            break;
-                        case 'A':
-                            nums.Add(14);
-                            break;
-                        default:
-                            break;
-                    }
-                }
-            }
-            nums.Sort();
-            return nums;
-        }
-        static List<List<int>> getOccurenceAndValues(List<int> nums)
-        {
-            List<List<int>> multiarray = new List<List<int>>();
-            int i = 0;
-            while (i < nums.Count)
-            {
-                List<int> row1 = new List<int>();
-                row1.Add((from temp in nums where temp.Equals(nums[i]) select temp).Count());
-                row1.Add(nums[i]);
-                multiarray.Add(row1);
-                i += (from temp in nums where temp.Equals(nums[i]) select temp).Count();
-            }
-            List<List<int>> values = multiarray.OrderBy(x => x[1]).OrderBy(y => y[0]).ToList();
-            values.Reverse();
-            return values;
         }
 
         //static long emod(long a, long b, long c)
