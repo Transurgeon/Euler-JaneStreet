8609d012a9c32edb109c8676e0be9458cee9df80
Transurgeon <peterzijie@gmail.com>
2023-01-26 19:27:14 -0500

    adding structure for BPuzzled in Manchester

--- New-Era-C++/Problem81.cpp
+++ New-Era-C++/Problem81.cpp
@@ -1,11 +1,34 @@
 
 #include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
 
 int main() {
     
-    for (int i = 0; i < 10; i++) {
-        
+    // Create a text string, which is used to output the text file
+    string myText;
+
+    // Read from the text file
+    ifstream MyReadFile("p081_matrix.txt");
+
+    // Use a while loop together with the getline() function to read the file line by line
+    while (getline (MyReadFile, myText)) {
+    // Output the text from the file
+    // cout << myText;
     }
-    std::cout << "Hello World!" << "Your counter is:";
+
+    // Close the file
+    MyReadFile.close();
+
+    // initialising the example 5 by 5 grid
+    int grid[5][5] = {
+        {131, 673, 234, 103, 18},
+        {201, 96, 342, 965, 150},
+        {630, 803, 746, 422, 111},
+        {537, 699, 497, 121, 956},
+        {805, 732, 524, 37, 331},
+    };
+
     return 0;
 }
