8834fd99a408e5faee4b2215b7aef6d6ac0a830b
William Zijie <peterzijie@gmail.com>
2024-01-12 22:05:27 -0500

    making progress on solving the small example with backtrack

--- Jane-Street/2024-01/Jan-2024.py
+++ Jane-Street/2024-01/Jan-2024.py
@@ -9,7 +9,7 @@ class State:
         self.grid = [[0] * size for _ in range(size)]
         self.rowSum = row_sum
         self.colSum = col_sum
-        self.lastCenter = (0,0)
+        self.lastCenters = [(0,0), (0,0)]
 
     def printState(self):
         """
@@ -162,11 +162,26 @@ class Solution:
         self.stack = [staty]
 
     def depthySolver(self):
-        three_centers = [(3,7)]
-        self.generate3_shapes(three_centers)
+        region6 = [(6, 3), (6, 4), (6, 5), (4, 2), (5, 2), (5, 3), (5, 5), (7, 4)]
+        region7 = [(4, 4), (4, 5), (4, 6), (4, 7), (3, 7), (3, 8), (2, 8), (5, 4)]
+        region17 = [(12, 8), (12, 9), (12, 10), (13, 10), (11, 9), (13, 8), (14, 8), (14, 7)]
+        region14 = [(9, 8), (9, 9), (10, 9), (10, 10), (11, 10), (11, 11), (12, 11), (12, 12)]
+        #self.generate3_shapes(region6 + region7)
+        #self.generate3_shapes(region17 + region14)
+
         count = 0
-        while count < 200000:
-            self.generateOthers()
+        s = self.stack.pop(0)
+        while count < 2000:
+            if self.generate2_shapes(s):
+                s = self.stack[0]
+            else:
+                if self.generate1_shapes(s):
+                    s = self.stack[0]
+                else:
+                    if not s.isSolution():
+                        s = self.stack.pop(0)
+                    else:
+                        return
             count += 1
             if count % 100 == 0:
                 self.stack[0].printState()
@@ -182,18 +197,27 @@ class Solution:
                     if (new_s.validateF_shape(m)):
                         self.stack.append(new_s)
 
-    def generateOthers(self): 
-        s = self.stack.pop(0)
-        row, col = s.lastCenter
+    def generate2_shapes(self, s):
+        row, col = s.lastCenters[1]
         for r in range(row, s.size):
             for c in range(0, s.size):
-                # print(r, c)
                 if s.validateCenter(Move(1, (r,c), 2)):
                     if self.generateFshapes(s, (r,c), 2) > 0:
-                        return
+                        return True
+        s.printState()
+        s.lastCenters[1] = (s.size - 1, s.size - 1)
+        return False
+
+    def generate1_shapes(self, s): 
+        row, col = s.lastCenters[0]
+        for r in range(row, s.size):
+            for c in range(0, s.size):
                 if s.validateCenter(Move(1, (r,c), 1)):
                     if self.generateFshapes(s, (r,c), 1) > 0:
-                        return
+                        return True
+        s.printState()
+        s.lastCenters[0] = (s.size - 1, s.size - 1)
+        return False
 
     def generateFshapes(self, s, p, val):
         count = 0
@@ -202,7 +226,7 @@ class Solution:
             m = Move(i, p, val)
             if (new_s.validateF_shape(m)):
                 count += 1
-                new_s.lastCenter = p
+                new_s.lastCenters[val-1] = p
                 self.stack.insert(0, new_s)
                 # new_s.printState()
         return count
@@ -225,15 +249,6 @@ def generateRangePoints(p1, p2):
     print(points)
 
 if __name__ == "__main__":
-    #row_sum = [3, 9, 10, math.inf, 13, 8, 7, 2]
-    #col_sum = [9, 12, 12, 10, 8, 8, math.inf, 2]
-
-    row_sum = [14, 24, 24, 39, 43, math.inf, 22, 23, 29, 28, 34, 35, 29, 26, 26, 24, 20]
-    col_sum = [13, 20, 22, 28, 30, 36, 35, 39, 49, 39, 39, math.inf, 23, 32, 23, 17, 13]
-
-    s = State(17, row_sum, col_sum)
-    sol = Solution(s)
-    # sol.depthySolver()
     region1 = [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0), (8, 0),
                (9, 0), (10, 0), (11, 0), (12, 0), (13, 0), (14, 0), (15, 0), (16, 0),
                (16, 1), (16, 2), (16, 3), (16, 4), (16, 5), (0, 1), (1, 1), (1, 2),
@@ -257,8 +272,7 @@ if __name__ == "__main__":
                 (16, 12), (15, 5), (15, 6), (14, 6)]
     region20 = [(15, 7), (15, 8), (15, 9), (15, 10), (15, 11), (15, 12),
                  (15, 13), (14, 12), (14, 13), (16, 13), (13, 9), (14, 9)]
-    region17 = [(12, 8), (12, 9), (12, 10), (13, 10), (11, 9), (13, 8),
-                (14, 8), (14, 7)]
+    region17 = [(12, 8), (12, 9), (12, 10), (13, 10), (11, 9), (13, 8), (14, 8), (14, 7)]
     region9 = [(5, 9), (6, 9), (7, 9), (7, 7), (8, 7), (9, 7), (7, 8),
                 (9, 6), (10, 6), (10, 5), (11, 5), (11, 4)]
     region10 = [(4, 10), (5, 10), (6, 10), (7, 10), (8, 10), (9, 10), 
@@ -280,4 +294,14 @@ if __name__ == "__main__":
                 (11, 11), (12, 11), (12, 12)]
     region18 = [(13, 11), (13, 12), (13, 13), (13, 14), (14, 10), (14, 11),
                 (14, 14), (14, 15), (11, 12), (11, 13), (12, 13)]
-    generateRangePoints((13, 11),(13, 14))
+    # generateRangePoints((13, 11),(13, 14))
+
+    row_sum = [3, 9, 10, math.inf, 13, 8, 7, 2]
+    col_sum = [9, 12, 12, 10, 8, 8, math.inf, 2]
+
+    #row_sum = [14, 24, 24, 39, 43, math.inf, 22, 23, 29, 28, 34, 35, 29, 26, 26, 24, 20]
+    #col_sum = [13, 20, 22, 28, 30, 36, 35, 39, 49, 39, 39, math.inf, 23, 32, 23, 17, 13]
+
+    s = State(8, row_sum, col_sum)
+    sol = Solution(s)
+    sol.depthySolver()
